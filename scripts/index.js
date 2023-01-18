a9d7e5dfbb8561068df520a70ffd493d3e744bf7
Gregor Lennie <gregorlennie@hotmail.co.uk>
2023-01-18 17:45:13 +0000

    Remove JS for sticky header and use CSS

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,1 @@
-window.onscroll = function () {
-  stickyHeader();
-};
 
-function stickyHeader() {
-  const header = document.getElementById("nav-header");
-  const sticky = header.offsetTop;
-
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
